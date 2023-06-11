c38c7a9458508dc1b9e25694e63d083f0f7af846
BrandanBurgess <brandanbb13@gmail.com>
2023-06-11 19:45:56 -0400

    bro

--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,68 @@
 public class Polynomial{
     double coefficients [];
+    int exponents [];
 
 
     public Polynomial(){
         this.coefficients = new double[] {0};
+        this.exponents = new int[] {0};
+
     }
 
-    public Polynomial(double[] sample){
+    public Polynomial(double[] sample, int[] sample2){
         this.coefficients = sample;
+        this.exponents = sample2;
     }
 
     public Polynomial add(Polynomial sample){
 
         Polynomial temp = new Polynomial();
 
-        if (this.coefficients.length > sample.coefficients.length){
-            temp.coefficients = this.coefficients;
-            for (int i = 0; i < sample.coefficients.length; i++) {
-                temp.coefficients[i] += sample.coefficients[i];
-            } 
-        }
+        int maxL = this.coeff.length + sample.coeff.length;
+		double[] new_coefficients = new double[length];
+		int[] new_exponents = new int[length];
 
-        else{
-            temp = sample;
-            for (int i = 0; i < this.coefficients.length; i++){
-                temp.coefficients[i] += this.coefficients[i];
-            }
-        }
+		
 
-        return temp;
+        int i = 0;
+        int p = 0; // k
+        int q = 0; // j
+
+		while (q < sample.coeff.length && i < coeff.length) {
+			if (this.powers[i] > sample.powers[q]) {
+				newCoeff[p] = sample.coeff[q];
+				newPowers[p] = sample.powers[q];
+				q++;
+			}
+
+            else if (this.powers[i] < p.powers[q]) {
+				newCoeff[p] = this.coeff[i];
+				newPowers[p] = this.powers[i];
+				i++;
+			}
+
+            else {
+				newCoeff[p] = this.coeff[i] + sample.coeff[q];
+				newPowers[p] = this.powers[i];
+				i++;
+				j++;
+			}
+			p++;
+		}
+
+		while (i < coeff.length) {
+			newCoeff[p] = this.coeff[i];
+			newPowers[p] = this.powers[i];
+			i++;
+			p++;
+		}
+
+		while (j < sample.coeff.length) {
+			newCoeff[p] = sample.coeff[q];
+			newPowers[p] = sample.powers[q];
+			q++;
+			p++;
+		}
 
     }
 
